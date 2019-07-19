61b03cbe138d77c06573af57246d394d507e2398
Teng YANG <teng.yang@oocl.com>
2019-07-19 14:10:23 +0800

    setup counter app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Counter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h2>{{count}}</h2>
+    <div>
+      <button @click="count++">点我+</button>
+      <button @click="count--">点我-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data(){
+    return {
+      count: 0
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
